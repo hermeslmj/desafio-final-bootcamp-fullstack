dcda085855cbc67ca795095fe3907fa921f2d19e
Hermes Luciano Monteiro Junior <you@example.com>
2020-07-10 22:37:03 -0300

    primeira versão do backend implementado

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,98 @@
 const express = require('express');
+const transactionService = require('../services/transactionService');
 const transactionRouter = express.Router();
 
+transactionRouter.post('/', async(req, res) => {
+    try {
+        const newTransaction = await transactionService.createTransaction(req.body);
+        res.send(newTransaction);
+    } catch (error) {
+        res.status(500).send({"message": error});
+    }
+});
+
+transactionRouter.put('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedTransaction = await transactionService.updateTransaction(id, req.body);
+        if(updatedTransaction) {
+            res.send(updatedTransaction);
+        }else{
+            res.status(500).send({"message": `Não foi possível atualizar a transaction de id ${id}`});    
+        }
+        
+    } catch (error) {
+        res.status(500).send({"message": error});
+    }
+});
+
+transactionRouter.get('/byId/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        if( id && id != "" ) {
+            const transaction = await transactionService.getTransactionById(id);
+            if(transaction) {
+                res.send(transaction);
+            }
+            else{
+                res.status(404).send({"message": `Transação não encontrada com o id: ${id}`});
+            }
+            
+        }
+        else{
+            res.status(500).send({"message": "Envio de um id é obrigatório"});
+        }
+        
+    }
+    catch(error) {
+        console.log(error);
+        res.status(500).send({"message": error});
+    }
+}) 
+
+transactionRouter.get('/:periodo', async (req, res) => {
+    try{
+        const periodo = req.params.periodo;
+        if( periodo && periodo != "" ) {
+            const transactions = await transactionService.getTransanctionsByPeriod(periodo);
+            res.send({length: transactions.length, transactions: transactions});
+        }
+        else{
+            res.status(500).send({"message": "Envio de um período é obrigatório"});
+        }
+        
+    }
+    catch(error) {
+        console.log(error);
+        res.status(500).send({"message": error});
+    }
+});
+
+
+transactionRouter.delete('/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        if( id && id != "" ) {
+            if(await transactionService.deleteTransactionById(id)){
+                res.send("Transação apagada com sucesso");
+            }
+            else
+            {
+                res.status(500).send(`Não foi possível apagar a transação com id: ${id}`);
+            }
+            
+        }
+        else{
+            res.status(500).send({"message": "Envio de um id é obrigatório"});
+        }
+        
+    }
+    catch(error) {
+        console.log(error);
+        res.status(500).send({"message": error});
+    }
+});
+
 module.exports = transactionRouter;
+
+
--- services/transactionService.js
+++ services/transactionService.js
@@ -6,3 +6,85 @@ const ObjectId = mongoose.Types.ObjectId;
 // o servidor é Linux, isso faz diferença. Gastei umas boas horas tentando
 // descobrir esse erro :-/
 const TransactionModel = require('../models/TransactionModel');
+
+async function createTransaction(data) {
+    try {
+        const newTransaction = new TransactionModel(data);
+
+        await newTransaction.save();
+        return newTransaction;
+
+    } catch (error) {
+        console.log(error);
+        throw "Problema ao criar Transaction";
+    }
+}
+
+async function getTransanctionsByPeriod(period) {
+    try {
+        const transactionList = await TransactionModel.find({ yearMonth: period });
+        return transactionList;
+    }
+    catch (err) {
+        console.log(err);
+        throw "Problema ao retornar lista de transações";
+    }
+
+    return [];
+}
+
+
+async function getTransactionById(id) {
+    try {
+        const transaction = await TransactionModel.findById(id);
+        if (transaction) {
+            return transaction;
+        }
+        else {
+            return false;
+        }
+
+    }
+    catch (err) {
+        console.log(err);
+        throw "Problema ao retornar lista de transações";
+    }
+
+    return null;
+}
+
+async function deleteTransactionById(id) {
+    try {
+        await TransactionModel.deleteOne({ _id: id });
+        return true;
+    }
+    catch (err) {
+        console.log(err);
+        throw "Problema ao retornar lista de transações";
+    }
+
+    return false;
+}
+
+
+async function updateTransaction(id, data) {
+    
+    try {
+        const updatedTransaction = await TransactionModel.findByIdAndUpdate({ _id: id }, data, { new: true });
+        if(updatedTransaction) {
+            return updatedTransaction;
+        }
+        else {
+            return false;
+        }
+
+    } catch (error) {
+        console.log(error);
+        throw "Problema ao editar Transaction";
+    }
+}
+
+
+
+
+module.exports = { getTransanctionsByPeriod, createTransaction, getTransactionById, deleteTransactionById, updateTransaction }
